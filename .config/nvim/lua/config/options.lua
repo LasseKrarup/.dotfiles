05a3b28d5f1091e21b7f0b21feb3d32b647a0250
Lasse Krarup <lk@eplusplus.dk>
2025-05-26 14:44:45 +0200

    Workaround for SSH clipboard being broken (NVIM)

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.opt.clipboard = "unnamedplus"
+
+-- Workaround for kinda broken tmux detection (it detects tmux clipboard when in ssh but no tmux running)
+if vim.env.SSH_TTY ~= nil and vim.env.TMUX == nil and vim.env.TERM ~= "tmux-256color" then
+  vim.g.clipboard = "osc52"
+end
