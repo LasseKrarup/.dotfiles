788713e477b32bfde7ed68edad4d8b048fff1837
Lasse Krarup <lk@eplusplus.dk>
2025-08-19 23:32:57 +0200

    Prettify whichkey groups

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -9,32 +9,34 @@ return {
     spec = {
       {
         mode = { "n", "v" },
-        { "<leader><tab>", group = "tabs" },
-        { "<leader>c", group = "code" },
-        { "<leader>d", group = "debug" },
-        { "<leader>dp", group = "profiler" },
-        { "<leader>f", group = "file/find" },
-        { "<leader>g", group = "git" },
-        { "<leader>gh", group = "hunks" },
-        { "<leader>q", group = "quit/session" },
-        { "<leader>s", group = "search" },
-        { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
-        { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
-        { "[", group = "prev" },
-        { "]", group = "next" },
-        { "g", group = "goto" },
-        { "gs", group = "surround" },
-        { "z", group = "fold" },
+        { "<leader><tab>", group = "Tabs" },
+        { "<leader>c", group = "Code" },
+        { "<leader>cm", group = "Commentbox" },
+        { "<leader>d", group = "Debug" },
+        { "<leader>dp", group = "Profiler" },
+        { "<leader>f", group = "Find..." },
+        { "<leader>g", group = "Git" },
+        { "<leader>gh", group = "[G]it [H]unks" },
+        { "<leader>q", group = "Quit/session" },
+        { "<leader>s", group = "Search..." },
+        { "<leader>t", group = "Typst..." },
+        { "<leader>u", group = "UI", icon = { icon = "󰙵 ", color = "cyan" } },
+        { "<leader>x", group = "Diagnostics/Quickfi[x]", icon = { icon = "󱖫 ", color = "green" } },
+        { "[", group = "Prev" },
+        { "]", group = "Next" },
+        { "g", group = "Goto" },
+        { "gs", group = "Surround" },
+        { "z", group = "Fold" },
         {
           "<leader>b",
-          group = "buffer",
+          group = "Buffer",
           expand = function()
             return require("which-key.extras").expand.buf()
           end,
         },
         {
           "<leader>w",
-          group = "windows",
+          group = "Windows",
           proxy = "<c-w>",
           expand = function()
             return require("which-key.extras").expand.win()
@@ -64,9 +66,5 @@ return {
   config = function(_, opts)
     local wk = require("which-key")
     wk.setup(opts)
-    if not vim.tbl_isempty(opts.defaults) then
-      LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
-      wk.register(opts.defaults)
-    end
   end,
 }
