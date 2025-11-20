f663c3f885fad889f61bcb8bc6a0847694109bfa
Lasse Krarup <latze95@gmail.com>
2025-11-20 15:28:43 +0100

    Add more fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias pandock='podman run --rm -v "$(pwd):/data" docker.io/pandoc/extra'
-alias lg='lazygit'
+alias pandock='docker run --rm -v "$(pwd):/data" docker.io/pandoc/extra'
+alias lg="lazygit"
+alias vi="nvim"
+alias vim="nvim"
 
 starship init fish | source
 zoxide init fish | source
